df0ce8112057978c4fcd514aa5be0a853f731650
Chris Williams <christopher.williams2@coxautoinc.com>
2021-04-04 11:25:20 -0500

    Changed string phrase

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        string phrase = "Something out of This world somethEyu FmLA I nEED";
+        string phrase = "Something out of This world that is smOoth";
         string[] words = phrase.Split(' ');
 
         string newsentence = null;
